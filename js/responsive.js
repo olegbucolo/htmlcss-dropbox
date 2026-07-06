b6f5250f0f7a61f39a60a27fa168e19cb6702df0
olegbucolo <olegbucolo97@gmail.com>
2026-07-07 01:13:09 +0200

    feature: fully responsive

---
+++ js/responsive.js
@@ -0,0 +1,8 @@
+const btnHeaderResponsive = document.querySelector('.btn-header-reponsive');
+const wrapperResponsive = document.querySelector('.wrapper-responsive');
+const btnHeaderReponsive = document.querySelector('.btn-header-reponsive');
+
+function toggleResponsiveHeader() {
+    wrapperResponsive.classList.toggle('active');
+    btnHeaderReponsive.classList.toggle('close');
+}
